e640dd889a11bd3ad98058339e790a693cd4fd8e
Prateek Singh <107751312+Prateekxe@users.noreply.github.com>
2023-03-15 21:20:03 +0530

    Create DLL - Add at End.c

---
+++ DSA/LINKED LIST/DLL - Add at End.c
@@ -0,0 +1,105 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *prev;
+    struct node *next;
+};
+
+struct node *head = NULL;
+
+void add_at_end()
+{
+    struct node *temp, *ptr;
+    temp = (struct node *)malloc(sizeof(struct node));
+    temp->prev = NULL;
+    printf("Enter the data: ");
+    scanf("%d", &temp->data);
+    temp->next = NULL;
+    ptr = head;
+    if(head == NULL)
+    {
+        head = temp;
+    }
+    else
+    {
+    while(ptr->next != NULL)
+    {
+        ptr = ptr->next;
+    }
+    ptr->next = temp;
+    temp->prev = ptr;
+    }
+    
+}
+
+void add_at_beg()
+{
+    struct node *temp;
+    temp = (struct node *)malloc(sizeof(struct node));
+    temp->prev = NULL;
+    printf("Enter the data: ");
+    scanf("%d", &temp->data);
+    temp->next = NULL;
+    
+    if(head == NULL)
+    {
+       head = temp;
+    }
+    else
+    {
+    temp->next = head;
+    head->prev = temp;
+    head = temp;
+    }
+}
+
+void display()
+{
+	if(head == NULL)
+	{
+		printf("Memory allocation failure");
+	}
+	else
+	{
+		struct node *ptr;
+		ptr = head;
+		while(ptr != NULL)
+		{
+		printf("%d \n", ptr->data);
+		ptr = ptr->next;
+	    }
+		
+	}
+}
+
+int main()
+{
+   int ch;
+   while(1)
+   {
+   	
+   	  printf("\n1. Insert at beginning\n");
+   	  printf("\n2. Insert at end\n");
+   	  printf("\n3. Display \n");
+   	  printf("\n4. Exit \n");
+   	  printf("\nchoose option: ");
+   	  scanf("%d",&ch);
+   	  switch(ch)
+   	{
+   	 	case 1: add_at_beg();
+   	 	break;
+   	 	case 2: add_at_end();
+   	 	break;
+   	 	case 3: display();
+   	 	break;
+   	 	case 4: exit(1);
+   	 	default:
+   	 	printf("wrong option taken");
+	}
+     
+    }
+}
+
+
